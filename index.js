004672c2c080fd5c6c4c18024ddf86861c1cd055
carloluis <carloluisr@gmail.com>
2019-02-16 23:55:21 +0100

    use media query hook function

---
+++ index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const mediaQueries = {};
+
+/**
+ * React hook to check when a valid media-query-string changes
+ * (i.e. when the media query test starts or stops evaluating to true)
+ *
+ * @param {string} mediaQueryString valid media query string. E.g.: `(min-width: 425px)`
+ * @returns {boolean} media-query matches value
+ */
+function useMediaQuery(mediaQueryString) {
+  const [queryMatch, setQueryMatch] = useState(null);
+
+  useEffect(() => {
+    function setMediaQueryMatchesHandler(e) {
+      setQueryMatch(e.matches);
+    }
+
+    if (!mediaQueries[mediaQueryString]) {
+      mediaQueries[mediaQueryString] = window.matchMedia(mediaQueryString);
+    }
+
+    const mediaQuery = mediaQueries[mediaQueryString];
+
+    mediaQuery.addListener(setMediaQueryMatchesHandler);
+
+    setMediaQueryMatchesHandler(mediaQuery);
+
+    return () =>
+      mediaQuery.removeListener(mediaQueryString, setMediaQueryMatchesHandler);
+  }, [mediaQueryString]);
+
+  return queryMatch;
+}
+
+export default useMediaQuery;
